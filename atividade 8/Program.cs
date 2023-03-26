2344ac5e683727ba9cc200977c85abba0f8bcae5
felip <felip@DESKTOP-0VT61A2>
2023-03-26 19:14:08 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OperacoesNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números inteiros:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("A - somar");
+            Console.WriteLine("S - subtrair");
+            Console.WriteLine("M - multiplicar");
+            Console.WriteLine("Q - quociente inteiro da divisão");
+            Console.WriteLine("E - sair");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao.ToUpper())
+            {
+                case "A":
+                    Console.WriteLine($"A soma é: {num1 + num2}");
+                    break;
+                case "S":
+                    Console.WriteLine($"A diferença é: {num1 - num2}");
+                    break;
+                case "M":
+                    Console.WriteLine($"O produto é: {num1 * num2}");
+                    break;
+                case "Q":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Erro: divisão por zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O quociente inteiro da divisão é: {num1 / num2}");
+                    }
+                    break;
+                case "E":
+                    Console.WriteLine("Programa encerrado.");
+                    break;
+                default:
+                    Console.WriteLine("Erro: opção inválida");
+                    break;
+            }
+        }
+    }
+}
